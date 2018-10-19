e0ca980522e541ea2ccf65ad7bc741b08e7daa72
Kévin Lejeune <lejeune-kevin@hotmail.fr>
2018-10-19 09:45:47 +0200

    add work

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+
+switch (true) {
+  case $("#products").click():
+    console.log("products");
+    break;
+  case $("#services").click():
+    console.log("services");
+    break;
+  case $("#software").click():
+    console.log("software");
+    break;
+  case $("#repair").click():
+    console.log("repair");
+    break;
+  case $("#sales").click():
+    console.log("sales");
+    break;
+}
