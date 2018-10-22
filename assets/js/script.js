db5085fe11c7810813d2ee10bd1e2eadc9ba07ce
Vallois Nicolas <nik-80@hotmail.fr>
2018-10-22 12:16:03 +0200

    ajout conditions incrémentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,10 +159,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit1 = produit1 - 10;
         $("#sousTotal1").val(produit1);
+        $("#mouseSmile").val(nmbrProduit1);
       } else {
         total = total - 10;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit1").hide();
+        $("#mouseSmile").val(0);
     }
     })
     $("#moins2").click(function(){
@@ -173,10 +175,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit2 = produit2 - 20;
         $("#sousTotal2").val(produit2);
+        $("#mouseCry").val(nmbrProduit2);
       } else {
         total = total - 20;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit2").hide();
+        $("#mouseCry").val(0);
       }
     })
     $("#moins3").click(function(){
@@ -187,10 +191,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit3 = produit3 - 50;
         $("#sousTotal3").val(produit3);
+        $("#mouseSDuck").val(nmbrProduit3)
       } else {
          total = total - 50;
          $("#maintenantFautRaquer").val(total);
          $("#displayProduit3").hide();
+         $("#mouseSDuck").val(0);
       }
     })
     $("#moins4").click(function(){
@@ -201,10 +207,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit4 = produit4 - 5;
         $("#sousTotal4").val(produit4);
+        $("#zeroFlouz").val(nmbrProduit4);
       } else {
         total = total - 5;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit4").hide();
+        $("#zeroFlouz").val(0);
       }
     })
     $("#moins5").click(function(){
@@ -215,10 +223,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit5 = produit5 - 1000;
         $("#sousTotal5").val(produit5);
+        $("#flouz").val(nmbrProduit5);
       } else {
         total = total - 1000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit6").hide();
+        $("#flouz").val(0);
       }
     })
     $("#moins6").click(function(){
@@ -229,10 +239,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit6 = produit6 - 5000;
         $("#sousTotal6").val(produit6);
+        $("#bigFlouz").val(nmbrProduit6);
       } else {
         total = total - 5000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit6").hide();
+        $("#bigFlouz").val(0);
       }
     })
     $("#moins7").click(function(){
@@ -243,10 +255,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit7 = produit7 - 50000;
         $("#sousTotal7").val(produit7);
+        $("#MWS").val(nmbrProduit7);
       } else {
         total = total - 50000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit7").hide();
+        $("#MWS").val(0);
       }
     })
     $("#moins8").click(function(){
@@ -257,10 +271,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit8 = produit8 - 100000;
         $("#sousTotal8").val(produit8);
+        $("#aI").val(nmbrProduit8);
       } else {
         total = total - 100000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit8").hide();
+        $("#aI").val(0);
       }
     })
     $("#moins9").click(function(){
@@ -271,10 +287,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit9 = produit9 - 110000;
         $("#sousTotal9").val(produit9);
+        $("#dO").val(nmbrProduit9);
       } else {
         total = total - 110000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit9").hide();
+        $("#dO").val(0);
       }
     })
     $("#moins10").click(function(){
@@ -285,10 +303,12 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit10 = produit10 - 140000;
         $("#sousTotal10").val(produit10);
+        $("#rI").val(nmbrProduit10);
       } else {
         total = total - 140000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit10").hide();
+        $("#rI").val(0);
       }
     })
     $("#moins11").click(function(){
@@ -299,99 +319,223 @@ $(function(){
         $("#maintenantFautRaquer").val(total);
         produit11 = produit11 - 100000;
         $("#sousTotal11").val(produit11);
+        $("#ACC").val(nmbrProduit11);
       } else {
         total = total - 100000;
         $("#maintenantFautRaquer").val(total);
         $("#displayProduit11").hide();
+        $("#ACC").val(0);
       }
     })
     $("#plus1").click(function(){
+      if (nmbrProduit1 <= 2) {
+        nmbrProduit1 ++;
+        $("#produitPanier1").val(nmbrProduit1);
+        total = total + 10;
+        $("#maintenantFautRaquer").val(total);
+        produit1 = produit1 + 10;
+        $("#sousTotal1").val(produit1);
+        $("#mouseSmile").val(nmbrProduit1);
+      }else{
       nmbrProduit1 ++;
       $("#produitPanier1").val(nmbrProduit1);
       total = total + 10;
       $("#maintenantFautRaquer").val(total);
       produit1 = produit1 + 10;
       $("#sousTotal1").val(produit1);
+      $("#mouseSmile").append(new Option(nmbrProduit1, nmbrProduit1));
+      $("#mouseSmile").val(nmbrProduit1);
+    }
     })
     $("#plus2").click(function(){
-      nmbrProduit2 ++;
-      $("#produitPanier2").val(nmbrProduit2);
-      total = total + 20;
-      $("#maintenantFautRaquer").val(total);
-      produit2 = produit2 + 20;
-      $("#sousTotal2").val(produit2);
+      if (nmbrProduit2 <= 2) {
+        nmbrProduit2 ++;
+        $("#produitPanier2").val(nmbrProduit2);
+        total = total + 20;
+        $("#maintenantFautRaquer").val(total);
+        produit2 = produit2 + 20;
+        $("#sousTotal2").val(produit2);
+      } else {
+        nmbrProduit2 ++;
+        $("#produitPanier2").val(nmbrProduit2);
+        total = total + 20;
+        $("#maintenantFautRaquer").val(total);
+        produit2 = produit2 + 20;
+        $("#sousTotal2").val(produit2);
+        $("#mouseCry").append(new Option(nmbrProduit2, nmbrProduit2));
+        $("#mouseCry").val(nmbrProduit2);
+      }
     })
     $("#plus3").click(function(){
-      nmbrProduit3 ++;
-      $("#produitPanier3").val(nmbrProduit3);
-      total = total + 50;
-      $("#maintenantFautRaquer").val(total);
-      produit3 = produit3 + 50;
-      $("#sousTotal3").val(produit3);
+      if (nmbrProduit3 <= 2) {
+        nmbrProduit3 ++;
+        $("#produitPanier3").val(nmbrProduit3);
+        total = total + 50;
+        $("#maintenantFautRaquer").val(total);
+        produit3 = produit3 + 50;
+        $("#sousTotal3").val(produit3);
+      } else {
+        nmbrProduit3 ++;
+        $("#produitPanier3").val(nmbrProduit3);
+        total = total + 50;
+        $("#maintenantFautRaquer").val(total);
+        produit3 = produit3 + 50;
+        $("#sousTotal3").val(produit3);
+        $("#mouseSDuck").append(new Option(nmbrProduit3, nmbrProduit3));
+        $("#mouseSDuck").val(nmbrProduit3);
+      }
     })
     $("#plus4").click(function(){
-      nmbrProduit4 ++;
-      $("#produitPanier4").val(nmbrProduit4);
-      total = total + 5;
-      $("#maintenantFautRaquer").val(total);
-      produit4 = produit4 + 5;
-      $("#sousTotal4").val(produit4);
+      if (nmbrProduit4 <= 2) {
+        nmbrProduit4 ++;
+        $("#produitPanier4").val(nmbrProduit4);
+        total = total + 5;
+        $("#maintenantFautRaquer").val(total);
+        produit4 = produit4 + 5;
+        $("#sousTotal4").val(produit4);
+      } else {
+        nmbrProduit4 ++;
+        $("#produitPanier4").val(nmbrProduit4);
+        total = total + 5;
+        $("#maintenantFautRaquer").val(total);
+        produit4 = produit4 + 5;
+        $("#sousTotal4").val(produit4);
+        $("#zeroFlouz").append(new Option(nmbrProduit4, nmbrProduit4));
+        $("#zeroFlouz").val(nmbrProduit4);
+      }
     })
     $("#plus5").click(function(){
-      nmbrProduit5 ++;
-      $("#produitPanier5").val(nmbrProduit5);
-      total = total + 1000;
-      $("#maintenantFautRaquer").val(total);
-      produit5 = produit5 + 1000;
-      $("#sousTotal5").val(produit5);
+      if (nmbrProduit5 <= 2) {
+        nmbrProduit5 ++;
+        $("#produitPanier5").val(nmbrProduit5);
+        total = total + 1000;
+        $("#maintenantFautRaquer").val(total);
+        produit5 = produit5 + 1000;
+        $("#sousTotal5").val(produit5);
+      } else {
+        nmbrProduit5 ++;
+        $("#produitPanier5").val(nmbrProduit5);
+        total = total + 1000;
+        $("#maintenantFautRaquer").val(total);
+        produit5 = produit5 + 1000;
+        $("#sousTotal5").val(produit5);
+        $("#flouz").append(new Option(nmbrProduit5, nmbrProduit5));
+        $("#flouz").val(nmbrProduit5);
+      }
     })
     $("#plus6").click(function(){
-      nmbrProduit6 ++;
-      $("#produitPanier6").val(nmbrProduit6);
-      total = total + 5000;
-      $("#maintenantFautRaquer").val(total);
-      produit6 = produit6 + 5000;
-      $("#sousTotal6").val(produit6);
+      if (nmbrProduit6 <= 2) {
+        nmbrProduit6 ++;
+        $("#produitPanier6").val(nmbrProduit6);
+        total = total + 5000;
+        $("#maintenantFautRaquer").val(total);
+        produit6 = produit6 + 5000;
+        $("#sousTotal6").val(produit6);
+      } else {
+        nmbrProduit6 ++;
+        $("#produitPanier6").val(nmbrProduit6);
+        total = total + 5000;
+        $("#maintenantFautRaquer").val(total);
+        produit6 = produit6 + 5000;
+        $("#sousTotal6").val(produit6);
+        $("#bigFlouz").append(new Option(nmbrProduit6, nmbrProduit6));
+        $("#bigFlouz").val(nmbrProduit6);
+      }
     })
     $("#plus7").click(function(){
-      nmbrProduit7 ++;
-      $("#produitPanier7").val(nmbrProduit7);
-      total = total + 50000;
-      $("#maintenantFautRaquer").val(total);
-      produit7 = produit7 + 50000;
-      $("#sousTotal7").val(produit7);
+      if (nmbrProduit7 <= 2) {
+        nmbrProduit7 ++;
+        $("#produitPanier7").val(nmbrProduit7);
+        total = total + 50000;
+        $("#maintenantFautRaquer").val(total);
+        produit7 = produit7 + 50000;
+        $("#sousTotal7").val(produit7);
+      } else {
+        nmbrProduit7 ++;
+        $("#produitPanier7").val(nmbrProduit7);
+        total = total + 50000;
+        $("#maintenantFautRaquer").val(total);
+        produit7 = produit7 + 50000;
+        $("#sousTotal7").val(produit7);
+        $("#MWS").append(new Option(nmbrProduit7, nmbrProduit7));
+        $("#MWS").val(nmbrProduit7);
+      }
     })
     $("#plus8").click(function(){
-      nmbrProduit8 ++;
-      $("#produitPanier8").val(nmbrProduit8);
-      total = total + 100000;
-      $("#maintenantFautRaquer").val(total);
-      produit8 = produit8 + 100000;
-      $("#sousTotal8").val(produit8);
+      if (nmbrProduit8) {
+        nmbrProduit8 ++;
+        $("#produitPanier8").val(nmbrProduit8);
+        total = total + 100000;
+        $("#maintenantFautRaquer").val(total);
+        produit8 = produit8 + 100000;
+        $("#sousTotal8").val(produit8);
+      } else {
+        nmbrProduit8 ++;
+        $("#produitPanier8").val(nmbrProduit8);
+        total = total + 100000;
+        $("#maintenantFautRaquer").val(total);
+        produit8 = produit8 + 100000;
+        $("#sousTotal8").val(produit8);
+        $("#aI").append(new Option(nmbrProduit8, nmbrProduit8));
+        $("#aI").val(nmbrProduit8);
+      }
     })
     $("#plus9").click(function(){
-      nmbrProduit9 ++;
-      $("#produitPanier9").val(nmbrProduit9);
-      total = total + 110000;
-      $("#maintenantFautRaquer").val(total);
-      produit9 = produit9 + 110000;
-      $("#sousTotal9").val(produit9);
+      if (nmbrProduit9 <= 2) {
+        nmbrProduit9 ++;
+        $("#produitPanier9").val(nmbrProduit9);
+        total = total + 110000;
+        $("#maintenantFautRaquer").val(total);
+        produit9 = produit9 + 110000;
+        $("#sousTotal9").val(produit9);
+      } else {
+        nmbrProduit9 ++;
+        $("#produitPanier9").val(nmbrProduit9);
+        total = total + 110000;
+        $("#maintenantFautRaquer").val(total);
+        produit9 = produit9 + 110000;
+        $("#sousTotal9").val(produit9);
+        $("#dO").append(new Option(nmbrProduit9, nmbrProduit9));
+        $("#dO").val(nmbrProduit9);
+      }
     })
     $("#plus10").click(function(){
-      nmbrProduit10 ++;
-      $("#produitPanier10").val(nmbrProduit10);
-      total = total + 140000;
-      $("#maintenantFautRaquer").val(total);
-      produit10 = produit10 + 140000;
-      $("#sousTotal10").val(produit10);
+      if (nmbrProduit10 <=2) {
+        nmbrProduit10 ++;
+        $("#produitPanier10").val(nmbrProduit10);
+        total = total + 140000;
+        $("#maintenantFautRaquer").val(total);
+        produit10 = produit10 + 140000;
+        $("#sousTotal10").val(produit10);
+      } else {
+        nmbrProduit10 ++;
+        $("#produitPanier10").val(nmbrProduit10);
+        total = total + 140000;
+        $("#maintenantFautRaquer").val(total);
+        produit10 = produit10 + 140000;
+        $("#sousTotal10").val(produit10);
+        $("#rI").append(new Option(nmbrProduit10, nmbrProduit10));
+        $("#rI").val(nmbrProduit10);
+      }
     })
     $("#plus11").click(function(){
-      nmbrProduit11 ++;
-      $("#produitPanier11").val(nmbrProduit11);
-      total = total + 100000;
-      $("#maintenantFautRaquer").val(total);
-      produit11 = produit11 + 100000;
-      $("#sousTotal11").val(produit11);
+      if (nmbrProduit11 <= 2) {
+        nmbrProduit11 ++;
+        $("#produitPanier11").val(nmbrProduit11);
+        total = total + 100000;
+        $("#maintenantFautRaquer").val(total);
+        produit11 = produit11 + 100000;
+        $("#sousTotal11").val(produit11);
+      } else {
+        nmbrProduit11 ++;
+        $("#produitPanier11").val(nmbrProduit11);
+        total = total + 100000;
+        $("#maintenantFautRaquer").val(total);
+        produit11 = produit11 + 100000;
+        $("#sousTotal11").val(produit11);
+        $("#ACC").append(new Option(nmbrProduit11, nmbrProduit11));
+        $("#ACC").val(nmbrProduit11);
+      }
     })
   })
   $("#pigeon").click(function(){
